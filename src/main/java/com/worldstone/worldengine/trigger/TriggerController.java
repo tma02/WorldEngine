001cd19c51ff7f3cc8c3035cf5f9858ee5e83da2
Tianyi Ma <tn.ma@outlook.com>
2018-07-21 00:42:31 -0500

    Don't warn on game_combat_tick not registered

--- src/main/java/com/worldstone/worldengine/trigger/TriggerController.java
+++ src/main/java/com/worldstone/worldengine/trigger/TriggerController.java
@@ -61,11 +61,10 @@ public class TriggerController {
         if (TriggerController.TRIGGER_MAP.containsKey(eventName)) {
             List<Trigger> eventTriggers = TriggerController.TRIGGER_MAP.get(eventName);
             for (Trigger trigger : eventTriggers) {
-                //new Thread(() -> trigger.onTrigger(attributes)).start();
                 trigger.onTrigger(attributes);
             }
         }
-        else {
+        else if (!eventName.equals("game_combat_tick")) {
             LoggerFactory.getLogger(TriggerController.class).warn("Triggered event !" + eventName + " has no registered triggers.");
         }
     }
